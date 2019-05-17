8493cb886a2b2f2875d8c1ed5adb6ac9b7efdbfc
Frans Orsel <theforsel@live.nl>
2019-05-17 13:49:58 +0200

    Created an AnswerModel

---
+++ Eduria/Eduria/Models/AnswerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduria.Models
+{
+    public class AnswerModel
+    {
+        public string Text;
+        public bool CorrectAnswer;
+
+        public AnswerModel(string text, bool correctAnswer)
+        {
+            this.Text = text;
+            this.CorrectAnswer = correctAnswer;
+        }
+    }
+}
