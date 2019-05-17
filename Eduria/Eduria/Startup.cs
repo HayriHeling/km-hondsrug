5cd801b3da58fc2dfec2e183c9bfed27760b77e0
HayriHeling <hayriheling@hotmail.com>
2019-05-17 13:46:28 +0200

    Revert "Init setup database"
    This reverts commit d5e6081791e1144e661b2bee21ee4079526d4c06.

--- Eduria/Eduria/Startup.cs
+++ Eduria/Eduria/Startup.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -29,9 +33,6 @@ namespace Eduria
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            //Add own DbContext and use Sql Server.
-            services.AddDbContext<EduriaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("EduriaDevelopment")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
