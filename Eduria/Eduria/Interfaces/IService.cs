9538638c1ea876ef9aa12b4e249278e9983f3eda
HayriHeling <hayriheling@hotmail.com>
2019-05-20 13:53:52 +0200

    Added IService

---
+++ Eduria/Eduria/Interfaces/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduria.Interfaces
+{
+    interface IService<T>
+    {
+        EduriaContext Context { get; set; }
+        IEnumerable<T> GetAll();
+        void Add(T item);
+        void Delete(T item);
+        T GetById(int id);
+    }
+}
