70e0a47a7035efb271c050dcd7171509d3747ff9
HayriHeling <hayriheling@hotmail.com>
2019-05-20 14:48:51 +0200

    Added an abstract service class Removed the interface

--- Eduria/EduriaData/Models/User.cs
+++ Eduria/EduriaData/Models/User.cs
@@ -7,8 +7,6 @@ namespace EduriaData.Models
     {
         [Required, MaxLength(45, ErrorMessage = "Vul een voornaam in.")]
         public string Firstname { get; set; }
-        [MaxLength(10)]
-        public string Infix { get; set; }
         [Required, MaxLength(45, ErrorMessage = "Vul een achternaam in.")]
         public string Lastname { get; set; }
         [Required, MaxLength(100, ErrorMessage = "Vul een e-mailadres in.")]
