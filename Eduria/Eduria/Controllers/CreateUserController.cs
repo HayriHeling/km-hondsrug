d92f8b79547315bb02a9202afd58d03ba6a79757
koendrenthe <koendrenthe8@hotmail.com>
2019-05-20 13:07:03 +0200

    added changes

---
+++ Eduria/Eduria/Controllers/CreateUserController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eduria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eduria.Controllers
+{
+    public class CreateUserController : Controller
+    {
+        // GET: CreateUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CreateUser/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CreateUser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CreateUser/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CreateUser/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CreateUser/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CreateUser/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CreateUser/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Eduria/Eduria/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduria.Models
+{
+    public class User
+    {        
+        public int UserId { get; set; }
+        [Display(Name = "Naam")]
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage="De Naam kan niet langer zijn dan 30 karakters.")]
+        [Required(ErrorMessage = "Dit veld is verplicht.")]
+        public string Name { get; set; }
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Voer een geldig email adres in.")]
+        [StringLength(128)]
+        [Required(ErrorMessage = "Dit veld is verplicht.")]
+        public string Email { get; set; }
+        [Display(Name = "Identificatie code")]
+        [Required(ErrorMessage = "Dit veld is verplicht.")]
+        public int UserNum { get; set; }
+        [Display(Name = "Klas code")]
+        [Required(ErrorMessage = "Dit veld is verplicht.")]
+        public int ClassId { get; set; }
+        [Display(Name = "Wachtwoord")]
+        [DataType(DataType.Password)]
+        [StringLength(255, MinimumLength = 8, ErrorMessage = "Wachtwoord moet minstens 8 karakters bevatten.")]
+        [Required(ErrorMessage = "Dit veld is verplicht.")]
+        public string Password { get; set; }
+
+    }
+}
