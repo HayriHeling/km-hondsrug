b9cdf84451003135f683d7041f0f286082989a6b
Adinda Knevel <knevel.adinda@gmail.com>
2019-05-21 10:35:32 +0200

    Added dashboard

--- Eduria/Eduria/Startup.cs
+++ Eduria/Eduria/Startup.cs
@@ -56,7 +56,7 @@ namespace Eduria
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Dashboard}/{action=Index}/{id?}");
             });
         }
     }
