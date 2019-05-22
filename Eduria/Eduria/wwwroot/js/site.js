504e113145ff80150025c260745c099ca93d0988
Adinda Knevel <knevel.adinda@gmail.com>
2019-05-22 10:40:19 +0200

    Removed js foundation, added logo + added responsive nav menu

--- Eduria/Eduria/wwwroot/js/site.js
+++ Eduria/Eduria/wwwroot/js/site.js
@@ -1,6 +1,15 @@
-﻿// Can also be used with $(document).ready()
+﻿// Flexslider
 $(window).load(function () {
     $('.flexslider').flexslider({
         animation: "slide"
     });
 });
+
+function responsiveToggle() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
