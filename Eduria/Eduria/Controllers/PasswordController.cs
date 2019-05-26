74469befdbcb1515ed91b01138b5fdb0196a1c59
adindaknevel <adinda@dotsolutions.nl>
2019-05-26 19:40:15 +0200

    Changed Forgot Password to a Model pop up

--- Eduria/Eduria/Controllers/PasswordController.cs
+++ Eduria/Eduria/Controllers/PasswordController.cs
@@ -40,55 +40,55 @@ namespace Eduria.Controllers
         }
 
         [HttpPost]
-        public ActionResult ForgotPassword(string Email)
-        {
-            Console.WriteLine("Testing!");
-            if (ModelState.IsValid)
-            {
-                string To = Email, UserID, Password, SMTPPort, Host;
-                string token = Guid.NewGuid().ToString();
+        //public ActionResult ForgotPassword(string Email)
+        //{
+        //    Console.WriteLine("Testing!");
+        //    if (ModelState.IsValid)
+        //    {
+        //        string To = Email, UserID, Password, SMTPPort, Host;
+        //        string token = Guid.NewGuid().ToString();
 
-                if (token != null)
-                {
-                    //Create URL with above token  
-                    var lnkHref = "<a href='" + Url.Action("Reset", "Password", new { code = token }, "https") + "'> Wachtwoord wijzigen</a>";
+        //        if (token != null)
+        //        {
+        //            //Create URL with above token  
+        //            var lnkHref = "<a href='" + Url.Action("Reset", "Password", new { code = token }, "https") + "'> Wachtwoord wijzigen</a>";
                     
-                    //HTML Template for Send email  
-                    string subject = "Verzoek om wachtwoord te wijzigen";
-                    string body = "Beste student, Klik op de link om je wachtwoord te resetten." + lnkHref;
+        //            //HTML Template for Send email  
+        //            string subject = "Verzoek om wachtwoord te wijzigen";
+        //            string body = "Beste student, Klik op de link om je wachtwoord te resetten." + lnkHref;
                     
-                    //Get and set the AppSettings using configuration manager.  
-                    EmailManager.AppSettings(out UserID, out Password, out SMTPPort, out Host);
-                    //return Content("UserID is: " + UserID);
-                    //Call send email methods.  
-                    EmailManager.SendEmail("info@adindatest3.nl", subject, body, Email, "info@adindatest3.nl", "wzRQ3Gg5mE", "465", "mail.axc.nl");
-                    return Content("Er is een mail met een link naar " + Email + " verzonden.");
+        //            //Get and set the AppSettings using configuration manager.  
+        //            EmailManager.AppSettings(out UserID, out Password, out SMTPPort, out Host);
+        //            //return Content("UserID is: " + UserID);
+        //            //Call send email methods.  
+        //            EmailManager.SendEmail("info@adindatest3.nl", subject, body, Email, "info@adindatest3.nl", "wzRQ3Gg5mE", "465", "mail.axc.nl");
+        //            return Content("Er is een mail met een link naar " + Email + " verzonden.");
 
 
-                    //// Token moet toegevoegd worden aan User
-                    //try
-                    //{
-                    //    User user = new User();
-                    //    UserService service = new UserService(this.ControllerContext);
-                    //    user = Service.GetUserByEmail(Email);
-                    //    user.Token = token;
-                    //    Service.Update(user);
-                    //    return Content("Er is een mail met een link naar " + Email + " verzonden.");
-                    //}
-                    //catch
-                    //{
-                    //    return Content("Er ging iets goed mis..");
-                    //}
-                }
-                else
-                {
-                    // If user does not exist or is not confirmed.  
-                    return View("Password");
+        //            //// Token moet toegevoegd worden aan User
+        //            //try
+        //            //{
+        //            //    User user = new User();
+        //            //    UserService service = new UserService(this.ControllerContext);
+        //            //    user = Service.GetUserByEmail(Email);
+        //            //    user.Token = token;
+        //            //    Service.Update(user);
+        //            //    return Content("Er is een mail met een link naar " + Email + " verzonden.");
+        //            //}
+        //            //catch
+        //            //{
+        //            //    return Content("Er ging iets goed mis..");
+        //            //}
+        //        }
+        //        else
+        //        {
+        //            // If user does not exist or is not confirmed.  
+        //            return View("Password");
 
-                }
-            }
-            return View("Password");
-        }
+        //        }
+        //    }
+        //    return View("Password");
+        //}
 
         public ActionResult Edit(string Token, string Password)
         {
