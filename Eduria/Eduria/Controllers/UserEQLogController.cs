48c84db60effa45add1fe67fc92191875acefff2
Frans Orsel <theforsel@live.nl>
2019-05-27 16:45:42 +0200

    Prepared Controller and Service for upcoming changes

---
+++ Eduria/Eduria/Controllers/UserEQLogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eduria.Services;
+using EduriaData.Models;
+
+namespace Eduria.Controllers
+{
+    public class UserEQLogController
+    {
+        private UserEQLogService _userEQLogService;
+
+        public UserEQLogController(UserEQLogService userEqLogService)
+        {
+            _userEQLogService = userEqLogService;
+        }
+
+        public void AddUserEQLog(UserEQLog userEqLog)
+        {
+            _userEQLogService.Add(userEqLog);
+        }
+    }
+}
