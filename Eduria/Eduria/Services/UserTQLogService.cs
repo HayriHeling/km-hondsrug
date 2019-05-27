8092bce49d5106ec238d409e47855ebcc83e7621
Jacco Wubs <jaccowubs@gmail.com>
2019-05-27 10:01:15 +0200

    Added service for UserTQLog

---
+++ Eduria/Eduria/Services/UserTQLogService.cs
@@ -0,0 +1,40 @@
+﻿using EduriaData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduria.Services
+{
+    public class UserTQLogService : AService<UserTQLog>
+    {
+        public UserTQLogService(EduriaContext context)
+        {
+            Context = context;
+        }
+
+        public override IEnumerable<UserTQLog> GetAll()
+        {
+            return Context.UserTQLogs;
+        }
+
+        public override UserTQLog GetById(int id)
+        {
+            return Context.UserTQLogs.Find(id);
+        }
+
+        public void AddLogs(int userId, int examId, IDictionary<int, int> log)
+        {
+            foreach (KeyValuePair<int, int> logItem in log)
+            {
+                Add(new UserTQLog
+                {
+                    UserId = userId,
+                    ExamId = examId,
+                    QuestionId = logItem.Key,
+                    TimesWrong = logItem.Value
+                });
+            }
+        }
+    }
+}
