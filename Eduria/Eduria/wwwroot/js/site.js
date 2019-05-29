0d0172a1963f80a9dad32aa41512820f2b8bd52b
Adinda Knevel <knevel.adinda@gmail.com>
2019-05-29 16:20:32 +0200

    Added basic slider to timperiods page

--- Eduria/Eduria/wwwroot/js/site.js
+++ Eduria/Eduria/wwwroot/js/site.js
@@ -1,10 +1,4 @@
-﻿// Flexslider
-$(window).load(function () {
-    $('.flexslider').flexslider({
-        animation: "slide"
-    });
-});
-
+﻿
 function responsiveToggle() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -13,3 +7,70 @@ function responsiveToggle() {
         x.className = "topnav";
     }
 }
+
+window.addEventListener('load', function () {
+    document.querySelector('.glider').addEventListener('glider-slide-visible', function (event) {
+        var glider = Glider(this);
+        console.log('Slide Visible %s', event.detail.slide)
+    });
+    document.querySelector('.glider').addEventListener('glider-slide-hidden', function (event) {
+        console.log('Slide Hidden %s', event.detail.slide)
+    });
+    document.querySelector('.glider').addEventListener('glider-refresh', function (event) {
+        console.log('Refresh')
+    });
+    document.querySelector('.glider').addEventListener('glider-loaded', function (event) {
+        console.log('Loaded')
+    });
+
+    window._ = new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1, //'auto',
+        slidesToScroll: 1,
+        itemWidth: 400,
+        draggable: true,
+        scrollLock: false,
+        dots: '#dots',
+        rewind: true,
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToScroll: 1,
+                    itemWidth: 400,
+                    slidesToShow: 'auto',
+                    exactWidth: true
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                    dots: false,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                    dots: false,
+                    arrows: false,
+                    scrollLock: true
+                }
+            }
+        ]
+    });
+});
