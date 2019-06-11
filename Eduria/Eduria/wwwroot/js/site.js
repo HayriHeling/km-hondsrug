cfedc71a4d0a961ead160934548fb97d4994eb7a
Adinda Knevel <knevel.adinda@gmail.com>
2019-06-11 14:05:26 +0200

    Removed jquery

--- Eduria/Eduria/wwwroot/js/site.js
+++ Eduria/Eduria/wwwroot/js/site.js
@@ -8,38 +8,6 @@ function responsiveToggle() {
     }
 }
 
-// Jquery
-
-//$(document).ready(function () {
-
-//    // Get the button that opens the modal
-//    var btn = $('#myBtn');
-
-//    // Get the <span> element that closes the modal
-//    var modal = $("#myModal");
-
-//    // Get the <span> element that closes the modal
-//    var span = $(".close")[0];
-
-//    //When the user clicks the button, open the modal 
-//    btn.click(function () {
-//        modal.css("display", "block");
-//    });
-
-//    // When the user clicks on <span> (x), close the modal
-//    span.click(function () {
-//        modal.css("display", "none");
-//    });
-
-//    // When the user clicks anywhere outside of the modal, close it
-//    window.click(function (event) {
-//        if (event.target === modal) {
-//            modal.css("display", "none");
-//        }
-//    });
-//});
-
-
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
