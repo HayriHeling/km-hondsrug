f8f74a7d36b54d6512f4dc673c9868cd96eb45bd
Frans Orsel <theforsel@live.nl>
2019-06-11 11:40:11 +0200

    Created 2 new models for showing the results of the exam per student per question

---
+++ Eduria/Eduria/Models/ExamPerStudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduria.Models
+{
+    public class ExamPerStudentModel
+    {
+        public ExamModel examModel { get; set; }
+        public List<UserEQLogModel> UserEqLogModels { get; set; }
+    }
+}
